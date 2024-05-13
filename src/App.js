0b92283e690cd71c8b42b52a37fcc3636b72e889
kiet-444 <kietcao4444@gmail.com>
2024-05-13 09:41:36 +0700

    init

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import Product from './Pages/Product';
 import LoginSignup from './Pages/LoginSignup';
 import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
+
 
 function App() {
   return (
@@ -17,11 +21,12 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopeCategory category="men"/>}/>
-        <Route path='/womens' element={<ShopeCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopeCategory category="kid"/>}/>
+        <Route path='/mens' element={<ShopeCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopeCategory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopeCategory banner={kid_banner} category="kid"/>}/>
         <Route path="/product" element={<Product/>}>
-        <Route path=':productId' element={<Product/>}/></Route>
+        <Route path=':productId' element={<Product/>}/>
+        </Route>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
